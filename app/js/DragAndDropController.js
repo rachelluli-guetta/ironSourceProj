c21b550f42dcc126e138821c3650609ffa7ac04c
RACHELG <rachelg@il.ibm.com>
2017-06-09 07:28:22 +0300

    Added img scaling

--- app/js/DragAndDropController.js
+++ app/js/DragAndDropController.js
@@ -2,9 +2,9 @@
  * Created by rachelg on 07/06/2017.
  */
 
-
 var dropZone = document.getElementById("dropZone");
 var thumbnailsContainer = document.getElementById("thumbnailsContainer");
+var IMG_SIZE = 100;
 
 //Change the UI when an image is dragged
 dropZone.addEventListener('dragover', function(e) {
@@ -30,30 +30,51 @@ dropZone.addEventListener('drop', function(e) {
     e.preventDefault();
     var files = e.dataTransfer.files; // Array of all files
 
-    console.log("Files are " + JSON.stringify(files));
+    for (var i = 0; i < files.length; i++ ) {
 
-    for (var key in files) {
+        var file = files[i];
+        var img = new Image();
 
-        var file = files[key];
+        img.onload = resizeImg;
+
         var reader = new FileReader();
 
-        reader.onload = function(e2) {
-            // finished reading file data.
-            var img = document.createElement('img');
+        reader.readAsDataURL(file);
 
-            img.src= e2.target.result;
-            thumbnailsContainer.appendChild(img);
+        reader.onload = function() {
+            img.src = reader.result;
         }
-
-        reader.readAsDataURL(file); // start reading the file data.
     }
 
     dropZone.classList.remove('element-dragged');
 });
 
 
-// function createThumbnailElement() {
-//     var img = document.createElement('img');
-//     img.classList.add('thumbnail');
-//     return img;
-// }
+function resizeImg() {
+    console.log('Resizing image..');
+
+    //this is bind to the img that is loading when the function is called
+    var img = this;
+    var canvas = document.createElement("canvas");
+
+    //Resize the img to fixed size 100x100px
+    canvas.width = IMG_SIZE;
+    canvas.height = IMG_SIZE;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, IMG_SIZE, IMG_SIZE);
+
+    var dataurl = canvas.toDataURL("image/png");
+    img.src= dataurl;
+
+    // window.localStorage.setItem( "image-base64", e.target.result );
+    createThumbnailElement(img);
+}
+
+
+function createThumbnailElement(img) {
+    var imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+    imgContainer.appendChild(img);
+    thumbnailsContainer.appendChild(imgContainer);
+}
--- app/js/ImageScaler.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by rachelg on 08/06/2017.
- */
