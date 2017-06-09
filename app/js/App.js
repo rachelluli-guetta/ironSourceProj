a8dcb3c73f675be55ef231f39ac03bb4f5956915
RACHELG <rachelg@il.ibm.com>
2017-06-09 14:40:59 +0300

    oops fix

--- app/js/App.js
+++ app/js/App.js
@@ -10,6 +10,5 @@ DragAndDropController.create(dropZone, onLoadImage);
 function onLoadImage() {
     var image = this;
     var resizedImage = ImageScaler.resizeImgAndCreateThumbnail(image, IMG_SIZE);
-    ThumbnailsController.addOrUpdateThumbnailElement(resizedImage);
-    Storge.store()
+    addOrUpdateThumbnailElement(resizedImage);
 }
